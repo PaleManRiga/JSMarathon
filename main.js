006dabc916b029de898c3a8ced571ac7155a5b2d
Sergejs <sergejs_rudenko@inbox.lv>
2020-09-23 12:18:39 +0300

    Homework 2

--- main.js
+++ main.js
@@ -1,7 +1,27 @@
 const str = "мама мыла раму";
+const str2 = "собака друг человека";
 
-
-
-for (let i = 0; i < str.length; i++) {
-    console.log(str.charAt(i));
+function getRow(firstRow, secondRow){
+    let firstCounter = 0;
+    let secondCounter = 0;
+    for (let i = 0; i < firstRow.length; i++){
+        if(firstRow.charAt(i) === "а"){
+            firstCounter++;
+        }
+    }
+    for (let i = 0; i < secondRow.length; i++){
+        if(secondRow.charAt(i) === "а"){
+            secondCounter++;
+        }
+    }
+    if(firstCounter > secondCounter){
+        console.log("first row has more letters a")
+    } else if (firstCounter < secondCounter){
+        console.log("second row has more letters a")
+    }
+    if(firstCounter === secondCounter){
+        console.log("a letters amount is equal")
+    }
 }
+
+console.log(getRow(str, str2));
