d1c99a66474ec6ffef292df496f880dfb9b35ef7
Sergejs <sergejs_rudenko@inbox.lv>
2020-09-28 16:30:42 +0300

    homework-4

--- main.js
+++ main.js
@@ -1,10 +1,40 @@
 const btn = document.getElementById('btn-kick');
+
+function renderProgressbarHP () {
+    this.elProgressbar.style.width = this.damageHP + '%'
+}
+
+function renderHPLife() {
+    this.elHP.innerText = this.damageHP + '/' + this.defaultHP;
+}
+
+function renderHP() {
+    this.renderHPLife;
+    this.renderProgressbarHP;
+}
+
+function changeHP(count) {
+    if (this.damageHP < count) {
+        this.damageHP = 0;
+        alert('Бедный ' + this.name + ' проиграл бой');
+        btn.disabled = true;
+    } else {
+       this.damageHP -= count;
+
+    }
+    this.renderHP;
+}
+
 const character = {
     name: 'Pikachu',
     defaultHP: 100,
     damageHP: 100,
     elHP: document.getElementById('health-character'),
     elProgressbar: document.getElementById('progressbar-character'),
+    renderProgressbarHP: renderProgressbarHP,
+    renderHPLife: renderHPLife,
+    renderHP: renderHP,
+    changeHP: changeHP,
 }
 
 const enemy = {
@@ -13,45 +43,23 @@ const enemy = {
     damageHP: 100,
     elHP: document.getElementById('health-enemy'),
     elProgressbar: document.getElementById('progressbar-enemy'),
+    renderProgressbarHP: renderProgressbarHP,
+    renderHPLife: renderHPLife,
+    renderHP: renderHP,
+    changeHP: changeHP,
 }
 btn.addEventListener('click', function () {
     console.log('Kick');
-    changeHP(random(20), character);
-    changeHP(random(20), enemy);
+    character.changeHP(random(20));
+    enemy.changeHP(random(20));
 })
 
 function init() {
     console.log('Start Game!');
-    renderHP(character);
-    renderHP(enemy);
+    character.renderHP;
+    enemy.renderHP;
 
 }
-
-function renderHPLife(person) {
-    person.elHP.innerText = person.damageHP + '/' + person.defaultHP;
-}
-
-function renderProgressbarHP(person) {
-    person.elProgressbar.style.width = person.damageHP + '%'
-}
-
-function renderHP(person) {
-    renderHPLife(person);
-    renderProgressbarHP(person);
-}
-
-function changeHP(count, person) {
-    if (person.damageHP < count) {
-        person.damageHP = 0;
-        alert('Бедный ' + person.name + ' проиграл бой');
-        btn.disabled = true;
-    } else {
-        person.damageHP -= count;
-        
-    }
-    renderHP(person);
-}
-
 function random(num) {
     return Math.ceil(Math.random() * num);
 }
